e8f6ff9bb8c84036b40a6f636ab1d10d12235ea1
Cody Scott <cody.j.b.scott@gmail.com>
2015-03-27 21:15:40 -0400

    Partial using Scrypt with JSPM

---
+++ config.js
@@ -0,0 +1,30 @@
+System.config({
+  "baseURL": "/",
+  "paths": {
+    "*": "*.js",
+    "github:*": "jspm_packages/github/*.js",
+    "npm:*": "jspm_packages/npm/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "scrypt": "npm:scrypt@4.0.7",
+    "github:jspm/nodelibs-path@0.1.0": {
+      "path-browserify": "npm:path-browserify@0.0.0"
+    },
+    "github:jspm/nodelibs-process@0.1.1": {
+      "process": "npm:process@0.10.1"
+    },
+    "npm:nan@1.7.0": {
+      "path": "github:jspm/nodelibs-path@0.1.0"
+    },
+    "npm:path-browserify@0.0.0": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:scrypt@4.0.7": {
+      "nan": "npm:nan@1.7.0"
+    }
+  }
+});
+
